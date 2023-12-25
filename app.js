0a575a5d46be58584747b88f97f53dfa6f53b544
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-25 22:36:09 +0500

    javascript task

---
+++ app.js
@@ -0,0 +1,7 @@
+function bulb_on(){
+    document.getElementById('bulb').src='https://i.postimg.cc/6QyTynzr/bulb-on.png';
+}
+function bulb_off(){
+    document.getElementById('bulb').src='https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+}
+ 
