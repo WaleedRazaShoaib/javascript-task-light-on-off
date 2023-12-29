0525516f10ac96796aa4a61c8ec011a463237b2f
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-29 12:58:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ function bulb_on(){
 function bulb_off(){
     document.getElementById('bulb').src='https://i.postimg.cc/KjK1wL3c/bulb-off.png';
 }
-var o = [1,2,3,4,5,6,7,8,9];
-let ret = o.forEach(i => {
-    console.log(i)
-});
+pop = "";
+function start(){
+    pop = 2
+}
  
